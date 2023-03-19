28328bcad57102a4ed73fdf7469f616389ea92b3
ShadowFox35 <anastasia97chernova@gmail.com>
2023-03-20 00:09:39 +0100

    fix: eslint errors

--- react-components/src/components/Main/Error/__tests__/index.test.tsx
+++ react-components/src/components/Main/Error/__tests__/index.test.tsx
@@ -1,12 +1,11 @@
 import { render } from '@testing-library/react';
 import * as React from 'react';
 import { testAppWrapper } from '../../../../constants/testsConst';
-
-import Catalog from '../Catalog';
+import Error from '../Error';
 
 describe('<testError />', () => {
   it('should render and match the snapshot', () => {
-    const componentRender = render(testAppWrapper(<Catalog />));
+    const componentRender = render(testAppWrapper(<Error />));
     expect(componentRender).toMatchSnapshot();
   });
 });
