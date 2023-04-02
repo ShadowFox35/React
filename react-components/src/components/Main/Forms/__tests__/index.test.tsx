93523f2e054d2f46b2fb95e3cf77c6e846f30967
ShadowFox35 <anastasia97chernova@gmail.com>
2023-04-03 00:15:20 +0200

    feat: some tests for forms

--- react-components/src/components/Main/Forms/__tests__/index.test.tsx
+++ react-components/src/components/Main/Forms/__tests__/index.test.tsx
@@ -6,5 +6,9 @@ describe('TextComponent', () => {
   it('renders the correct text', () => {
     const { getByText } = render(<Forms />);
     expect(getByText('Add new item')).toBeInTheDocument();
+    expect(getByText('Product type:')).toBeInTheDocument();
+    expect(getByText('Product Name:*')).toBeInTheDocument();
+    expect(getByText('Delivery')).toBeInTheDocument();
+    expect(getByText('Receipt date:*')).toBeInTheDocument();
   });
 });
